511bd476c1d2ea52e0d807976da663f49202a374
brenda-m <brendanjuguna1@gmail.com>
2024-02-24 11:38:54 +0300

    define postgres port

--- app/jobs/link_statistics_job.rb
+++ app/jobs/link_statistics_job.rb
@@ -1,15 +1,16 @@
 class LinkStatisticsJob < ApplicationJob
-
   queue_as :default
-
-  after_perform do |job|
-    self.class.set(wait: 5.minutes).perform_later(job.arguments.first)
-  end
 
   def perform(user_id)
     user = User.find(user_id)
-    urls = user.urls.map { |url| {original_url: url.original_url, short_link: url.short_link, click: url.click } }
+    urls = user.urls.map { |url| { original_url: url.original_url, short_link: url.short_link, click: url.click } }
     StatsMailer.stats_email(user, urls).deliver_later
-    self.class.set(wait: 5.minutes).perform_later(user.id)
+    reschedule_job(user.id)
+  end
+
+  private
+
+  def reschedule_job(user_id)
+    self.class.set(wait: 5.minutes).perform_later(user_id)
   end
 end
