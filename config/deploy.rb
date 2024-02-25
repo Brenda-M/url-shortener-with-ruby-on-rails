d063575545fba2f6c67ae1775b570138cab0cd18
brenda-m <brendanjuguna1@gmail.com>
2024-02-26 01:34:41 +0300

    add prod evironment to deploy

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,5 +1,5 @@
-# # config valid for current version and patch releases of Capistrano
-# lock "~> 3.18.0"
+# config valid for current version and patch releases of Capistrano
+lock "~> 3.18.0"
 
 # set :application, "snappy"
 # set :repo_url, "git@github.com:Brenda-M/url-shortener-with-ruby-on-rails.git"
@@ -26,36 +26,12 @@
 # set :puma_worker_timeout, nil
 # set :puma_init_active_record, true
 # set :puma_preload_app, false
-# config valid for current version and patch releases of Capistrano
-lock "~> 3.16.0"
-set :application, "snappy"
-set :repo_url, "git@github.com:Brenda-M/url-shortener-with-ruby-on-rails.git"
+set :application, 'snappy'
+set :repo_url,  "git@github.com:Brenda-M/url-shortener-with-ruby-on-rails.git"
+set :branch, :main
 set :deploy_to, '/home/dev/snappy'
-set :use_sudo, true
-set :branch, 'main'
-set :linked_files, %w{config/master.key config/database.yml}
-set :rails_env, 'production'
-set :keep_releases, 2
-set :linked_dirs, fetch(:linked_dirs, []).push('log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'vendor/bundle', 'public/system')
-set :linked_files, %w{config/database.yml config/master.key}
-# Default branch is :master
-# ask :branch, `git rev-parse --abbrev-ref HEAD`.chomp
-set :puma_bind,       "unix://#{shared_path}/tmp/sockets/#{fetch(:application)}-puma.sock"
-set :puma_state,      "#{shared_path}/tmp/pids/puma.state"
-set :puma_pid,        "#{shared_path}/tmp/pids/puma.pid"
-set :puma_access_log, "#{release_path}/log/puma.access.log"
-set :puma_error_log,  "#{release_path}/log/puma.error.log"
-set :ssh_options,     { forward_agent: true, user: fetch(:user), keys: %w(~/.ssh/id_rsa.pub) }
-set :puma_preload_app, true
-set :puma_worker_timeout, nil
-set :puma_init_active_record, true  # Change to false when not using ActiveRecord
-namespace :puma do
-desc 'Create Directories for Puma Pids and Socket'
-task :make_dirs do
-on roles(:app) do
-execute "mkdir #{shared_path}/tmp/sockets -p"
-execute "mkdir #{shared_path}/tmp/pids -p"
-end
-end
-before :start, :make_dirs
-end
+set :pty, true
+set :puma_env, fetch(:rack_env, fetch(:rails_env, 'production'))
+set :linked_files, %w{config/database.yml config/application.yml}
+set :linked_dirs, %w{log tmp/pids tmp/cache tmp/sockets vendor/bundle public/system public/uploads}
+set :keep_releases, 5
