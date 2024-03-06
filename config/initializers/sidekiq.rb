3a3dc6f4e3b54dfbae4e64bac93dfc45ac682068
brenda-m <brendanjuguna1@gmail.com>
2024-03-06 21:12:13 +0300

    add redis env variable

--- config/initializers/sidekiq.rb
+++ config/initializers/sidekiq.rb
@@ -2,17 +2,28 @@
 
 
 
-Sidekiq.configure_server do |config|
-  config.redis = { url: 'redis://127.0.0.1:6379/0' }
-end
-
-Sidekiq.configure_client do |config|
-  config.redis = { url: 'redis://127.0.0.1:6379/0' }
-end
 # Sidekiq.configure_server do |config|
-#   config.redis = { url: 'redis://redis:6379/0' }
+#   config.redis = { url: 'redis://127.0.0.1:6379/0' }
 # end
 
 # Sidekiq.configure_client do |config|
-#   config.redis = { url: 'redis://redis:6379/0' }
+#   config.redis = { url: 'redis://127.0.0.1:6379/0' }
 # end
+# # Sidekiq.configure_server do |config|
+# #   config.redis = { url: 'redis://redis:6379/0' }
+# # end
+
+# # Sidekiq.configure_client do |config|
+# #   config.redis = { url: 'redis://redis:6379/0' }
+# # end
+
+
+redis_url = ENV['REDIS_URL'] || 'redis://127.0.0.1:6379/0'
+
+Sidekiq.configure_server do |config|
+  config.redis = { url: redis_url }
+end
+
+Sidekiq.configure_client do |config|
+  config.redis = { url: redis_url }
+end
